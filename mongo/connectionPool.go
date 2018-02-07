30887a6c42348427944b8e138abd1d01fe6aa1f5
icechen <icechen128@gmail.com>
2018-02-07 16:53:46 +0800

    更改 mongodb 依赖库

--- mongo/connectionPool.go
+++ mongo/connectionPool.go
@@ -1,26 +1,36 @@
 package mongo
 
-import (
-	"gopkg.in/mgo.v2"
-)
+import "github.com/globalsign/mgo"
 
 // Mongodb encapsulation
 type Mongodb struct {
 	mgoSession *mgo.Session
 	url        string
+	user       string
+	password   string
+
+	session *mgo.Session
 }
 
 // NewMongodb creat new mongodb
-func NewMongodb(url string) *Mongodb {
+func NewMongodb(url string, user string, password string) *Mongodb {
 	mgodb := new(Mongodb)
 	mgodb.url = url
+	mgodb.user = user
+	mgodb.password = password
 	return mgodb
 }
 
 func (m *Mongodb) getSession() *mgo.Session {
 	if m.mgoSession == nil {
+		info := &mgo.DialInfo{
+			Addrs:    []string{m.url},
+			Username: m.user,
+			Password: m.password,
+			Source:   "test",
+		}
 		var err error
-		m.mgoSession, err = mgo.Dial(m.url)
+		m.mgoSession, err = mgo.DialWithInfo(info)
 		if err != nil {
 			panic(err)
 		}
@@ -37,8 +47,15 @@ func (m *Mongodb) c(dbname string, collection string) *mgo.Collection {
 }
 
 func (m *Mongodb) insert(dbname string, collection string, document interface{}) error {
-	temSession := m.getSession()
-	defer temSession.Close()
-	temC := temSession.DB(dbname).C(collection)
+	if m.session == nil {
+		m.session = m.getSession()
+	}
+	temC := m.session.DB(dbname).C(collection)
 	return temC.Insert(document)
+}
+
+func (m *Mongodb) Close() {
+	if m.session != nil {
+		m.session.Close()
+	}
 }
